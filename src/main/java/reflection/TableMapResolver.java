42f865e395c6c9f4704d003bfe8a29caa36514c1
uraven0107 <turn.pso@gmail.com>
2021-05-20 20:50:08 +0900

    :pencil: コミット漏れ追加

--- src/main/java/reflection/TableMapResolver.java
+++ src/main/java/reflection/TableMapResolver.java
@@ -1,16 +1,33 @@
 package reflection;
 
+import anno.TableAnnotation;
+import core.TableMap;
+import io.github.classgraph.*;
 import object.Table;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
 
 public class TableMapResolver {
-    public TableMapResolver run() {
-        return this;
-    }
 
-    public Map<String, Table> getMap() {
-        return new HashMap<>();
-    }
+   public void run(final TableMap tableMap) {
+       try (ScanResult result = new ClassGraph().enableClassInfo().enableAnnotationInfo().scan()) {
+           ClassInfoList classInfos = result.getClassesWithAnnotation(TableAnnotation.class.getName());
+           classInfos.forEach(classInfo -> {
+               AnnotationInfo annotationInfo = classInfo.getAnnotationInfo(TableAnnotation.class.getName());
+               AnnotationParameterValueList paramValues = annotationInfo.getParameterValues();
+               String tableName = (String) paramValues.get("name").getValue();
+               // TODO 型のチェックをする
+               @SuppressWarnings("unchecked")
+               Class<Table> tableClass = (Class<Table>)classInfo.loadClass();
+               Table table = null;
+               try {
+                   table = tableClass.getDeclaredConstructor().newInstance();
+               } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                   // TODO 例外処理を考える
+                   e.printStackTrace();
+               }
+               tableMap.register(tableName, table);
+           });
+       }
+   }
 }
